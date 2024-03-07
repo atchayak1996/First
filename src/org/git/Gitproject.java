cb58f00e74f2f516e7f6cbf541663caaa7f8a291
atchayak1996 <atchayak1996@gmail.com>
2024-03-07 13:27:26 +0530

    added few lines

---
+++ src/org/git/Gitproject.java
@@ -0,0 +1,11 @@
+package org.git;
+
+public class Gitproject {
+private void sample() {
+	System.out.println("hai hello");
+}
+public static void main(String[] args) {
+	Gitproject ob=new Gitproject();
+	ob.sample();
+}
+}
