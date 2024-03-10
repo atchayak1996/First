b0d16015a1e960f2cafc7cd787e3352ad99cd6cf
atchayak1996 <atchayak1996@gmail.com>
2024-03-10 08:58:55 +0530

    added code

--- src/org/git/Gitproject.java
+++ src/org/git/Gitproject.java
@@ -3,6 +3,7 @@ package org.git;
 public class Gitproject {
 private void sample() {
 	System.out.println("hai hello");
+	System.out.println("kayan addded his code");
 }
 public static void main(String[] args) {
 	Gitproject ob=new Gitproject();
