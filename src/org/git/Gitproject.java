712a118dd3be6d13ece9ed47ca5ee955e8d92639
atchayak1996 <atchayak1996@gmail.com>
2024-03-10 07:24:59 +0530

    kayira added the code

--- src/org/git/Gitproject.java
+++ src/org/git/Gitproject.java
@@ -3,6 +3,7 @@ package org.git;
 public class Gitproject {
 private void sample() {
 	System.out.println("hai hello");
+	System.out.println("hai this is kayira");
 }
 public static void main(String[] args) {
 	Gitproject ob=new Gitproject();
